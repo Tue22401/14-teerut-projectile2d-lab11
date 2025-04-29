a4b350abacd9464b1226cd24e94a6660c031fd5a
Tue22401 <Teerut.muan@bumail.net>
2025-04-29 16:04:17 +0700

    final

--- Assets/Scripts/Projectile2d.cs
+++ Assets/Scripts/Projectile2d.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 public class Projecti : MonoBehaviour
@@ -9,6 +10,37 @@ public class Projecti : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
+
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            if (hit.collider != null)
+            {
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log("hit"+ hit.collider.name);
+
+                Vector2 projecttileVelocity = CalculateProjectVelocity(shootPoint.position, hit.point, 1f);
+                Rigidbody2D shootBullet = Instantiate(bulletPrefab, shootPoint.position , Quaternion.identity);
+                shootBullet.linearVelocity = projecttileVelocity;
+
+                
+                
+            }
+        }
         
     }
+
+    Vector2 CalculateProjectVelocity(Vector2 origin, Vector2 target, float time)
+    {
+
+    Vector2 distance = target - origin;
+    float velocityX = distance.x / time;
+    float velocityY = distance.x / time + 0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
+    Vector2 projectileVelocity = new Vector2(velocityX, velocityY);
+    return projectileVelocity;
+    }
 }
+  
